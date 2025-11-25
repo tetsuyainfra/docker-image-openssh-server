83d2e49ee5b64f412a6e84783b481b1b2f6ab922
tetsuyainfra <tetsuyainfra@gmail.com>
2025-11-26 00:32:54 +0900

    Makefileとtest.shを更新し、docker runコマンドの環境変数を整理。test.shにSSHログインとファイル転送のテストを追加。

--- Makefile
+++ Makefile
@@ -54,14 +54,13 @@ run: $(BUILD_TIMESTAMP_FILE)
 		-e INIT_CREATE_DIRS='$(TEST_INIT_CREATE_DIRS)' \
 		$(BUILD_IMAGE_NAME)
 
-start: $(BUILD_TIMESTAMP_FILE)
+run2: $(BUILD_TIMESTAMP_FILE)
 	docker run --name $(TEST_CONTAINER_NAME) \
+		--rm \
+		-v ./test-vars.yml:/test-vars.yml:ro \
 		-p 2222:$(TEST_INIT_SSHD_PORT) \
 		-e DEBUG=$(DEBUG) \
-		-e INIT_GROUPS='$(TEST_INIT_GROUPS)' \
-		-e INIT_USERS='$(TEST_INIT_USERS)' \
-		-e INIT_SSHD_CONFIG='$(TEST_INIT_SSHD_CONFIG)' \
-		-e INIT_CREATE_DIRS='$(TEST_INIT_CREATE_DIRS)' \
+		-e INIT_CONFIG='/test-vars.yml' \
 		$(BUILD_IMAGE_NAME)
 
 vars:
@@ -70,37 +69,8 @@ vars:
 	@echo '$(TEST_INIT_SSHD_CONFIG)'
 	@echo '$(TEST_INIT_CREATE_DIRS)'
 
-# test: $(BUILD_TIMESTAMP_FILE)
-# 	docker run --rm -d --name $(TEST_CONTAINER_NAME) -p 3142:3142 \
-# 		-e TOP_MIRROR_DEBIAN='$(TOP_MIRROR_DEBIAN)' \
-# 		-e TOP_MIRROR_UBUNTU='$(TOP_MIRROR_UBUNTU)' \
-# 		-e USE_CRON=TRUE \
-# 		-e CRON_SCHEDULE="* 4 * * *" \
-# 		-e ALLOW_USER_PORTS="80 443" \
-# 		-e ADD_REPOS='$(TEST_ADD_REPOS)' \
-# 		acng:latest
-# 	PROXY=localhost:3142 ./test.sh || { \
-# 		echo "Tests failed." ; \
-# 		docker stop $(TEST_CONTAINER_NAME) ; \
-# 		exit 1; \
-# 	};
-# 	docker stop $(TEST_CONTAINER_NAME)
-
-
-# shell: $(BUILD_TIMESTAMP_FILE)
-# 	docker exec -it acng-testing /bin/bash
-
-
-# just_run: $(BUILD_TIMESTAMP_FILE)
-# 	docker run --rm --name acng-testing -p 3142:3142 acng:latest
-
-
-# conf_acng: $(BUILD_TIMESTAMP_FILE)
-# # Copy default config files from the image to local directory ./tmp
-# # but /etc/apt-cacher-ng include link files
-# 	CONTAINER_ID=$$(docker create acng:latest) ; \
-# 	docker container cp -aL $$CONTAINER_ID:/etc/apt-cacher-ng ./tmp ; \
-# 	docker container rm $$CONTAINER_ID
+test: $(BUILD_TIMESTAMP_FILE)
+	./test.sh
 
 cp_ssh_config: $(BUILD_TIMESTAMP_FILE)
 	@./scripts/cp-image-file.sh $(BUILD_IMAGE_NAME) /etc/ssh ./tmp
--- test.sh
+++ test.sh
@@ -1,18 +1,82 @@
 #!/bin/bash
 
+BUILD_IMAGE_NAME="openssh-server:latest"
+INIT_GROUPS=$(yq -c .groups test-vars.yml)
+INIT_USERS=$(yq -c .users test-vars.yml)
+INIT_CREATE_DIRS=$(yq -c '.create_dirs' test-vars.yml)
+INIT_SSHD_PORT=$(yq -c .sshd_config.Port test-vars.yml)
+INIT_SSHD_CONFIG=$(yq -c .sshd_config test-vars.yml)
 
-echo -n  > ./test_known_hosts
-ssh  -F ./test_ssh_config -o PreferredAuthentications=password example1@openssh-server-testing
+if [ "$DEBUG" = "1" ] || [ "$DEBUG" = "TRUE" ] || [ "$DEBUG" = "true" ]; then
+    DEBUG_ON=1
+    set -x
+else
+    DEBUG_ON=0
+fi
 
+cleanup() {
+    echo "テストに使ったコンテナ削除します..."
+	docker stop $CONTAINER_NAME || true
+}
+trap cleanup EXIT
 
-# ssh -F ./test_ssh_config -o IdentityFile=./test_key_ed25519 -o IdentitiesOnly=yes example2@openssh-server-testing
+set -e
 
-	docker run --name $(TEST_CONTAINER_NAME) \
-		--rm \
-		-p 2222:$(TEST_INIT_SSHD_PORT) \
-		-e DEBUG=$(DEBUG) \
-		-e INIT_GROUPS='$(TEST_INIT_GROUPS)' \
-		-e INIT_USERS='$(TEST_INIT_USERS)' \
-		-e INIT_SSHD_CONFIG='$(TEST_INIT_SSHD_CONFIG)' \
-		-e INIT_CREATE_DIRS='$(TEST_INIT_CREATE_DIRS)' \
-		$(BUILD_IMAGE_NAME)
+CONTAINER_NAME=$(docker run  \
+	--detach \
+	--rm \
+	-p 2222:$INIT_SSHD_PORT \
+	-e DEBUG=$DEBUG \
+	-e INIT_GROUPS="$INIT_GROUPS" \
+	-e INIT_USERS="$INIT_USERS" \
+	-e INIT_SSHD_CONFIG="$INIT_SSHD_CONFIG" \
+	-e INIT_CREATE_DIRS="$INIT_CREATE_DIRS" \
+	$BUILD_IMAGE_NAME)
+echo CONTAINER_NAME: $CONTAINER_NAME
+
+sleep 3
+
+{
+	echo "Generating test_known_hosts"
+	ssh-keyscan -p 2222 localhost | tee ./test_known_hosts
+}
+
+{
+	echo example1 should be able to login
+	r=$(sshpass -p "password1" ssh -F test_ssh_config -o PreferredAuthentications=password example1@openssh-server-testing echo "example1 login successful")
+	if [ "$r" != "example1 login successful" ]; then
+	  echo "example1 login failed"
+	  exit 1
+	fi
+}
+
+{
+	echo example2 should NOT be able to login
+	set +e
+	ssh -F ./test_ssh_config -o IdentityFile=./test_key_ed25519 -o IdentitiesOnly=yes example2@openssh-server-testing
+	if [ $? -eq 0 ]; then
+	  echo "example2 ssh login MUST fail but succeeded"
+	  exit 1
+	fi
+}
+
+{
+	echo example2 should be able to copy file via scp
+	scp -F ./test_ssh_config -o IdentityFile=./test_key_ed25519 -o IdentitiesOnly=yes \
+		./README.md \
+		example2@openssh-server-testing:/data
+	if [ $? -ne 0 ]; then
+	  echo "example2 scp MUST succeed but failed"
+	  exit 1
+	fi
+
+	echo "Verifying copied file content"
+	docker cp $CONTAINER_NAME:/chroot/example2/data/README.md ./tmp/README_copied.md
+	diff ./README.md ./tmp/README_copied.md
+	if [ $? -ne 0 ]; then
+	  echo "Copied file content does not match"
+	  exit 1
+	fi
+}
+
+echo "###### All tests passed successfully. #######"
